9f2bc5501b89fcd3aee245bea937e26a60dd60c2
Evert Vanderstadt <evert.vanderstadt.w9991@student.hogent.be>
2018-09-12 20:57:11 +0200

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const MongoClient = mongodb.MongoClient;
 var db;
